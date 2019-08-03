5242f044c002e6a633637b9001b6e6541997fbb8
sureshkumar007 <sureshkumar12074@gmail.com>
2019-08-03 10:26:12 +0530

    conditional statements created

---
+++ README.md
@@ -0,0 +1,12 @@
+Conditional Statements and Loops
+Arrays
+OOPs Concepts - Object, Class, Interface, Inheritance, Package
+Language Basics - Variables, Operators, Expressions, Statements and Blocks, Control Flow Statements, Enums
+More on Classes and Objects - Anatomy of a class, Constructors, Creating and using objects, this keyword, Controlling access to a member, Nested Classes, When to use nested, inner, local, anonymus classes and Lambda expressions.
+More on Interfaces and Inheritance - Define and Implement an interface, Using an interface as a type, Default Methods, Multiple inheritance of State, Implementationa and Type, Overriding and hiding methods, Polymorphism, Hiding fields, super keyword, Final classes and methods, Abstract methods and classes
+Numbers and Strings - The Number classes, Math class, String class, conversion between numbers and string, Comparing strings, StringBuilder Class, Autoboxing and unboxing
+Exception handling - try, catch, finally blocks, Throw exceptions, Creating custom exception class, chained exceptions, Advantages of exceptions.
+Basic I/O - I/O Streams using java.io package (Byte, Character, Buffered Stream, Scanning and formating, Data streams),  File I/O using java.nio.file package (Path class, File operations etc)
+Collections - Collection framework, collection interfaces, Implementations, Aggregate operations, Algorithms
+Generics - Types, Methods, Type Inference, Wildcards, Type erasure, Restrictions on Generics
+Lambda Expressions and Streams (Java 8 features)
---
+++ src/main/java/com/stackroute/javakeyword/ConditionalAndLooping.java
@@ -0,0 +1,72 @@
+package com.stackroute.javakeyword;
+
+import java.util.Scanner;
+
+public class ConditionalAndLooping
+{
+    public static void main(String args[]) {
+        /*
+        Taking local variables
+         */
+        int num;
+        int review;
+        char ch;
+        System.out.println("Enter number to check even or odd");
+        Scanner scanner = new Scanner(System.in);
+        num = scanner.nextInt();
+        evenOrOddCheck(num);
+        feedBackTaking(num);
+        /*
+        Creating object using Class name
+         */
+//        ConditionalAndLooping conditionalAndLooping=new ConditionalAndLooping();
+//         /*
+//         Calling two methods;;;;
+//          */
+//        conditionalAndLooping.evenOrOddCheck(num);
+//        conditionalAndLooping.feedBackTaking(num);
+
+
+    }
+/*
+Even or odd checking method
+ */
+    public static void evenOrOddCheck(int num) {
+
+
+        if (num > 0) {
+            if (num % 2 == 0) {
+                System.out.println(num + "Even number");
+            } else {
+                System.out.println(num + "odd number");
+            }
+        }
+    }
+    /*
+    Taking feedback from the user
+     */
+    public static void feedBackTaking(int num)
+    {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Plz give feed back 1->Good, 2->Average, 3->Bad");
+        num=scanner.nextInt();
+        switch (num)
+        {
+            case 1:
+                System.out.println("Thank you");
+                break;
+            case 2:
+                System.out.println("Thank you and we will happy next time");
+                break;
+            case 3:
+                System.out.println("Sorry for this,");
+                break;
+            default:
+                System.out.println("plz enter between 1 And 3");
+
+        }
+
+
+    }
+}
+
